3ec0b22512b90709ab8fae080da6119bad20e7cc
manoj7git <43413379+manoj7git@users.noreply.github.com>
2021-05-25 23:24:51 +0530

    VRT Project updated from MainLap

--- src/test/java/com/vrt/testcases/testRunner.java
+++ src/test/java/com/vrt/testcases/testRunner.java
@@ -23,14 +23,9 @@ public class testRunner {
 		ExtentReporterNG er = new ExtentReporterNG();
 
 		testng = new TestNG();
-		//testng.setTestClasses(new Class[] {assetCreationTest.class});
-		testng.setTestClasses(new Class[] {HitNTrialTests.class});
-		//testng.setTestClasses(new Class[] {LoginTest.class});
-		//testng.setTestClasses(new Class[] {UserManagementTest.class, UserPrivilagesTest.class, UM_customized_UserPrivilagesTest.class});
-		//testng.setTestClasses(new Class[] {assetCreationTest.class, assetHubTest.class, assetHubAssetCreationTest.class});
-		//testng.setTestClasses(new Class[] {LoginTest.class, assetDetailsTest.class});
-		//testng.addListener(er);
-		//testng.addListener(er);
+		//testng.setTestClasses(new Class[] {HitNTrialTests.class});
+		testng.setTestClasses(new Class[] {QualificationProcessTest.class});
+
 		testng.run();			
 	}
 
